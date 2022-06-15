eba2bd3abda082349f69b881ae312eb8c57aa806
MarcusAnglin <21136754+MarcusAnglin@users.noreply.github.com>
2022-06-15 00:25:45 -0400

    General quiz app complete

--- src/context.js
+++ src/context.js
@@ -9,10 +9,6 @@ const table = {
 
 const API_ENDPOINT = "https://opentdb.com/api.php?";
 
-const url = "";
-const tempUrl =
-  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
-
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
@@ -83,11 +79,16 @@ const AppProvider = ({ children }) => {
   };
 
   const handleChange = (event) => {
-    console.log(event);
+    const name = event.target.name;
+    const value = event.target.value;
+    setQuiz({ ...quiz, [name]: value });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const { amount, category, difficulty } = quiz;
+    const url = `${API_ENDPOINT}amount=${amount}&category=${table[category]}&difficulty=${difficulty}&type=multiple`;
+    fetchQuections(url);
   };
 
   return (
